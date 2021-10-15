5a5eefc558a8520995b63dbc6f644d14098d9cb8
Belkis Vasquez <Belkis Vasquez@YAZMINGVP>
2021-10-14 22:35:32 -0400

    Mantenimiento asignaturas

---
+++ CalculoIndice/Controllers/AsignaturasController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CalculoIndice.Models;
+
+namespace CalculoIndice.Controllers
+{
+    public class AsignaturasController : Controller
+    {
+        private CalculoIndiceEntities db = new CalculoIndiceEntities();
+
+        // GET: Asignaturas
+        public ActionResult Index()
+        {
+            var asignatura = db.Asignatura.Include(a => a.Asignatura2).Include(a => a.Asignatura3).Include(a => a.Profesores);
+            return View(asignatura.ToList());
+        }
+
+        // GET: Asignaturas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asignatura asignatura = db.Asignatura.Find(id);
+            if (asignatura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asignatura);
+        }
+
+        // GET: Asignaturas/Create
+        public ActionResult Create()
+        {
+            ViewBag.CoRequisitos = new SelectList(db.Asignatura, "AsignaturaId", "Clave");
+            ViewBag.PreRequisitos = new SelectList(db.Asignatura, "AsignaturaId", "Clave");
+            ViewBag.ProfesoresId = new SelectList(db.Profesores, "ProfesoresId", "Nombre");
+            return View();
+        }
+
+        // POST: Asignaturas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AsignaturaId,Clave,Nombre,Credito,PreRequisitos,CoRequisitos,ProfesoresId")] Asignatura asignatura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Asignatura.Add(asignatura);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CoRequisitos = new SelectList(db.Asignatura, "AsignaturaId", "Clave", asignatura.CoRequisitos);
+            ViewBag.PreRequisitos = new SelectList(db.Asignatura, "AsignaturaId", "Clave", asignatura.PreRequisitos);
+            ViewBag.ProfesoresId = new SelectList(db.Profesores, "ProfesoresId", "Nombre", asignatura.ProfesoresId);
+            return View(asignatura);
+        }
+
+        // GET: Asignaturas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asignatura asignatura = db.Asignatura.Find(id);
+            if (asignatura == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CoRequisitos = new SelectList(db.Asignatura, "AsignaturaId", "Clave", asignatura.CoRequisitos);
+            ViewBag.PreRequisitos = new SelectList(db.Asignatura, "AsignaturaId", "Clave", asignatura.PreRequisitos);
+            ViewBag.ProfesoresId = new SelectList(db.Profesores, "ProfesoresId", "Nombre", asignatura.ProfesoresId);
+            return View(asignatura);
+        }
+
+        // POST: Asignaturas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AsignaturaId,Clave,Nombre,Credito,PreRequisitos,CoRequisitos,ProfesoresId")] Asignatura asignatura)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(asignatura).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CoRequisitos = new SelectList(db.Asignatura, "AsignaturaId", "Clave", asignatura.CoRequisitos);
+            ViewBag.PreRequisitos = new SelectList(db.Asignatura, "AsignaturaId", "Clave", asignatura.PreRequisitos);
+            ViewBag.ProfesoresId = new SelectList(db.Profesores, "ProfesoresId", "Nombre", asignatura.ProfesoresId);
+            return View(asignatura);
+        }
+
+        // GET: Asignaturas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asignatura asignatura = db.Asignatura.Find(id);
+            if (asignatura == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asignatura);
+        }
+
+        // POST: Asignaturas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Asignatura asignatura = db.Asignatura.Find(id);
+            db.Asignatura.Remove(asignatura);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
